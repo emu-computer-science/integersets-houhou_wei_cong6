d828d0a873ec66d7a95275e7364346d7f8f37228
Brian Cong <brian.cong12345@gmail.com>
2024-02-23 23:56:01 -0500

    Branch and commit sort

--- src/SetOfIntegers.java
+++ src/SetOfIntegers.java
@@ -1,8 +1,12 @@
-
+import java.util.*;
 
 class SetOfIntegers {
-public int reverse(boolean inOrder) {
-	System.out.println("we are testing reverse");
-	return 0;
-}
+	ArrayList<Integer> setOfIntegers = new ArrayList<Integer>();
+	public int reverse(boolean inOrder) {
+		System.out.println("we are testing reverse");
+		return 0;
+	}
+	public int sort(boolean reverse = true) {
+		System.out.println("Sorting SetOfIntegers")
+	}
 }

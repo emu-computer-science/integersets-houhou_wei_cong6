724bf204e397ecc9b6ad41af6b4f2a274f36b0b2
Brian Cong <brian.cong12345@gmail.com>
2024-02-24 00:23:11 -0500

    add compareTo function

--- src/SetOfIntegers.java
+++ src/SetOfIntegers.java
@@ -24,4 +24,22 @@ class SetOfIntegers {
         return sort(false);
     }
     
+    public int length() {
+    	return setOfIntegers.size();
+    }
+    
+    //default comparison function
+    //returns the difference in size between the two sets -- set1 - set2
+    //negative when s1 < s2, positive when s1>s2, 0 when s1=s2
+    public int compareTo(SetOfIntegers o) {
+    	int s = 0;
+    	for(int i = 0; i < this.length(); i++) {
+    		s += this.setOfIntegers.get(i);
+    	}
+        for(int i = 0; i < o.length(); i++) {
+        	s -= o.setOfIntegers.get(i);
+        }
+        return s;
+    }
+    
 }

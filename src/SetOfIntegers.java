daaea6491761c5e0d4fd6de5ca7fe105801f3ca5
Brian Cong <brian.cong12345@gmail.com>
2024-02-24 00:10:45 -0500

    Added fully fleshed out sort functions

--- src/SetOfIntegers.java
+++ src/SetOfIntegers.java
@@ -1,13 +1,27 @@
 import java.util.*;
 
 class SetOfIntegers {
-	ArrayList<Integer> setOfIntegers = new ArrayList<Integer>();
+	public ArrayList<Integer> setOfIntegers = new ArrayList<Integer>();
+	
 	public int reverse(boolean inOrder) {
 		System.out.println("we are testing reverse");
 		return 0;
 	}
-	public int sort(boolean reverse = false) {
-		System.out.println("Sorting SetOfIntegers")
-		
+	
+	//full sort function
+	public ArrayList<Integer> sort(boolean reverse) {
+		System.out.println("Sorting SetOfIntegers");
+		if(reverse) {
+			Collections.sort(setOfIntegers);
+		}else {
+			Collections.sort(setOfIntegers, Collections.reverseOrder());
+		}
+		return setOfIntegers;
 	}
+	
+	//default function call is false without parameters
+    public ArrayList<Integer> sort() {
+        return sort(false);
+    }
+    
 }
